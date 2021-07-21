68738a2b594431a10c0acc3325b710db31d69a9c
Sudhanshu <bsudhanshu3@gmail.com>
2021-07-21 23:45:03 +0530

    made navbar responsive for mobile screen size

--- src/Header.js
+++ src/Header.js
@@ -7,6 +7,11 @@ function Header() {
   const [introActive, setintroActive] = useState("active")
   const [projectsActive, setprojectsActive] = useState("")
   const [contactActive, setcontactActive] = useState("")
+  const [sidebar, setsidebar] = useState("sidebarClose")
+
+  const sidebarOpen = () => {
+    setsidebar("sidebarClose")
+  }
 
   const toggleHeaderBg = () => {
     let header = document.getElementById("header")
@@ -47,12 +52,12 @@ function Header() {
     // eslint-disable-next-line
   }, []);
 
-  const scrollto = (id) => {
+  const scrollto = id => {
     let header = document.getElementById("header")
 
     if (id === "projects") {
       window.scrollTo({
-        top: getTopOffset(id) - header.offsetHeight,
+        top: getTopOffset(id) - (window.innerWidth > 599 ? header.offsetHeight : 0),
         behavior: "smooth"
       })
     } else {
@@ -61,14 +66,28 @@ function Header() {
         behavior: "smooth"
       })
     }
+
+    setsidebar("sidebarClose")
   }
 
   return (
-    <div className={`header ${renderBg}`} id="header">
-      <div onClick={() => scrollto("intro")} className={introActive}>Home</div>
-      <div onClick={() => scrollto("projects")} className={projectsActive}>Projects</div>
-      <div onClick={() => scrollto("contact")} className={contactActive}>Contact</div>
-    </div >
+    <>
+      <div className={`header ${renderBg} ${sidebar}`} id="header">
+        <div onClick={() => scrollto("intro")} className={introActive} role="button">Home</div>
+        <div onClick={() => scrollto("projects")} className={projectsActive} role="button">Projects</div>
+        <div onClick={() => scrollto("contact")} className={contactActive} role="button">Contact</div>
+        <div className="header__closeButton" onClick={sidebarOpen}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+            <path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z" />
+          </svg>
+        </div>
+      </div>
+      <div className="header__menuIcon" onClick={() => setsidebar("sidebarOpen")}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+        </svg>
+      </div>
+    </>
   )
 }
 
